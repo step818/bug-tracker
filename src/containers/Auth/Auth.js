99bec88b9393971b5f9ef7870dd3966873bed4d8
Stephen Trewick <stephent818@gmail.com>
2020-04-20 13:38:48 -0700

    init

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  render(){
+    return(
+      <div>
+        Signin / Create account
+      </div>
+    );
+  }
+};
+
+export default Auth;
