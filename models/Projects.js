f311102aafb0c8de329367abf50243152bedb7c8
Stephen Trewick <stephent818@gmail.com>
2020-05-01 11:31:02 -0700

    project model init, and create project route

---
+++ models/Projects.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  dueDate: {
+    type: Date
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  goals: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      priority: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      },
+      description: {
+        type: String
+      },
+      status: {
+        type: String
+      }
+    }
+  ],
+  team: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      firstName: {
+        type: String
+      },
+      lastName: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      role: {
+        type: String
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      firstName: {
+        type: String
+      },
+      lastName: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+});
+
+module.exports = Project = mongoose.model('project', ProjectSchema);
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-//@route Get api/projects
-//@desc Test route
+const Project = require('../../models/Projects');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+//@route  Post api/projects
+//@desc   Create a project
 //@access Public
-router.get('/', (req, res) => res.send('Projects route'));
+router.post(
+  '/',
+  [
+    auth, 
+    [
+      check('text', 'Text is required')
+      .not()
+      .isEmpty()
+    ]
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newProject = new Project ({
+        text: req.body.text,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const project = await newProject.save();
+
+      res.json(project);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
