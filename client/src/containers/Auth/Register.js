96793719d49cbaa6f8ab4c8766cd0df1810e5f2a
Stephen Trewick <stephent818@gmail.com>
2020-05-04 12:24:50 -0700

    useState in register container

--- client/src/containers/Auth/Register.js
+++ client/src/containers/Auth/Register.js
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+  // Set state useing hooks (useState)
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { firstName, lastName, email, password, password2 } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  }
+
   return (
     <div>
-      Register
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder=" First Name" 
+            name="firstName" 
+            value={firstName}
+            onChange={e => onChange(e)}
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder=" Last Name" 
+            name="lastName" 
+            value={lastName}
+            onChange={e => onChange(e)}
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email Address" 
+            name="email" 
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
     </div>
   )
 }
