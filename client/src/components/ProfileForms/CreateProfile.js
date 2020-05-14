234a8621697e66407fcd26a75470398858cc37e7
Stephen Trewick <stephent818@gmail.com>
2020-05-14 11:13:27 -0700

    createProfile action

--- client/src/components/ProfileForms/CreateProfile.js
+++ client/src/components/ProfileForms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
+import { Link, withRouter } from 'react-router-dom';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     location: '',
@@ -18,8 +20,6 @@ const CreateProfile = props => {
   });
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
-
-  
 
   const {
     company,
@@ -37,6 +37,11 @@ const CreateProfile = props => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">
@@ -47,7 +52,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -145,6 +150,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
   profile: PropTypes.object
 }
 
@@ -152,4 +158,4 @@ const mapStateToProps = state => ({
   profile: state.profile
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));
